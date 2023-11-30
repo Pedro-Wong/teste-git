5ae119a6a2c885adab1dc4e79c32ac5cbd4c8bc6
Pedro Henrique de Azevedo Wong Rego <phenrique.wong@gmail.com>
2023-11-30 13:37:43 -0300

    Exercicio fibonacci

--- teste.js
+++ teste.js
@@ -1,19 +1,48 @@
-var valores = [];
-var x = 0;
-var y = 0;
-for (var cont = 0; cont < 8; cont++) {
-    var entrada = Number(prompt("Entre com o pre\u00E7o do ".concat(cont + 1, " produto")));
+/*
+let valores = [];
+let x = 0;
+let y = 0;
+
+for(let cont = 0; cont < 8; cont++){
+    let entrada = Number(prompt(`Entre com o preço do ${cont +1} produto`));
     valores[cont] = entrada;
-}
-;
-for (var controle = 0; controle < valores.length - 1; controle++) {
-    var carrega = valores[0];
-    if (carrega < valores[controle + 1]) {
-        x = valores[valores.length - 1];
-        valores[valores.length - 1] = valores[controle + 1];
-        valores[controle + 1] = x;
+};
+
+
+
+for(let controle = 0; controle < valores.length-1; controle++){
+    let carrega = valores[0];
+    if(carrega < valores[controle + 1]){
+        x = valores[valores.length-1]
+        valores[valores.length-1] = valores[controle + 1];
+        valores[controle +1] = x
         console.log(valores);
-    }
-    ;
-}
-;
+    };
+};
+*/
+// Exercicio 71
+/*
+let vetorx = [];
+
+for(let incre = 0; incre < 8; incre++){
+     vetorx[incre] = Math.round(Math.random()*10)
+};
+
+console.log(vetorx);
+
+*/
+// exercicio 72 (Fibonacci)
+/*
+let vetor4 = [];
+
+for(let incre = 0; incre < 15; incre++){
+    vetor4[incre] = Math.round(Math.random()*15);
+};
+
+for(let incre = 2; incre < 15; incre++){
+   
+    vetor4[incre] = vetor4[incre - 2] + vetor4[incre - 1];
+};
+
+console.log(vetor4);
+*/
--- teste.ts
+++ teste.ts
@@ -1,3 +1,4 @@
+/*
 let valores = [];
 let x = 0;
 let y = 0;
@@ -18,3 +19,39 @@ for(let controle = 0; controle < valores.length-1; controle++){
         console.log(valores);
     };
 };
+*/
+
+// Exercicio 71
+/*
+let vetorx = [];
+
+for(let incre = 0; incre < 8; incre++){
+     vetorx[incre] = Math.round(Math.random()*10)
+};
+
+console.log(vetorx);
+
+*/
+
+// exercicio 72 (Fibonacci)
+
+/*
+let vetor4 = [];
+
+for(let incre = 0; incre < 15; incre++){
+    vetor4[incre] = Math.round(Math.random()*15);
+};
+
+for(let incre = 2; incre < 15; incre++){
+   
+    vetor4[incre] = vetor4[incre - 2] + vetor4[incre - 1];
+};
+
+console.log(vetor4);
+*/
+
+
+
+
+
+
