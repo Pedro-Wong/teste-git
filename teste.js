7a711eb21b81c3ce8bc0390c671050c7e025d78b
Pedro Henrique de Azevedo Wong Rego <phenrique.wong@gmail.com>
2023-11-30 00:06:31 -0300

    30112023

--- teste.js
+++ teste.js
@@ -1,7 +1,19 @@
 var valores = [];
+var x = 0;
+var y = 0;
 for (var cont = 0; cont < 8; cont++) {
     var entrada = Number(prompt("Entre com o pre\u00E7o do ".concat(cont + 1, " produto")));
     valores[cont] = entrada;
 }
 ;
-console.log(valores);
+for (var controle = 0; controle < valores.length - 1; controle++) {
+    var carrega = valores[0];
+    if (carrega < valores[controle + 1]) {
+        x = valores[valores.length - 1];
+        valores[valores.length - 1] = valores[controle + 1];
+        valores[controle + 1] = x;
+        console.log(valores);
+    }
+    ;
+}
+;
--- teste.ts
+++ teste.ts
@@ -1,7 +1,20 @@
 let valores = [];
+let x = 0;
+let y = 0;
 
 for(let cont = 0; cont < 8; cont++){
     let entrada = Number(prompt(`Entre com o preço do ${cont +1} produto`));
-    valores[cont] = entrada
+    valores[cont] = entrada;
 };
-console.log(valores)
+
+
+
+for(let controle = 0; controle < valores.length-1; controle++){
+    let carrega = valores[0];
+    if(carrega < valores[controle + 1]){
+        x = valores[valores.length-1]
+        valores[valores.length-1] = valores[controle + 1];
+        valores[controle +1] = x
+        console.log(valores);
+    };
+};
